253cb3a6c34b2e73e80d78779504bb65606c3671
Cristian <cefunck@miuandes.cl>
2019-04-20 16:52:19 -0400

    ready => Complete the login flow for a platform user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@ class ApplicationController < ActionController::Base
   def index
 
   end
-
-  def current_user
-	#complete this method
-  end
+  private
+    def current_user
+      @_current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
+    end
 
   def is_user_logged_in?
-	#complete this method
-  	logged_in = false
-	if logged_in then true else redirect_to root_path end 
+  	logged_in = (current_user != nil)
+  	if logged_in then true else redirect_to root_path end
   end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,30 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		#complete this method
+		credentials = session_params
+		if user = authenticate(credentials[:email], credentials[:password])
+			session[:current_user_id] = user.id
+			flash[:notice] = "You have successfully logged in."
+			redirect_to user_path(session[:current_user_id])
+		else
+			flash[:notice] = "Error: Invalid credentials."
+			redirect_to root_url
+		end
 	end
 
 	def destroy
-		#complete this method
+		session[:current_user_id] = nil
+		flash[:notice] = "You have successfully logged out."
+		redirect_to root_url
 	end
+
+	private
+		def session_params
+			params.require(:session).permit(:email, :password)
+		end
+
+		def authenticate(email, password)
+			user = User.where(["email = ? and password = ?", email, password])[0]
+		end
+
 end
