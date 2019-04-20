ddd92130254ca4921dbcf916fe842b85b063f324
Cristian <cefunck@miuandes.cl>
2019-04-20 00:36:59 -0400

    Ready => Complete the registration for an user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,22 @@
 class RegistrationsController < ApplicationController
 	def new
-	
+
 	end
 
 	def create
-	    #complete this method
+		@user = User.new(registration_params)
+    if @user.save
+			flash[:notice] = 'User was successfully registered.'
+			redirect_to root_url
+    else
+			flash[:notice] = @user.errors.messages
+			redirect_to :registrations
+    end
 	end
+
+	private
+		def registration_params
+			params.require(:registrations).permit(:name, :last_name, :email, :phone, :password)
+		end
+
 end
